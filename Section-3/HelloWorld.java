41ab28fc65c7a49f7a75f5d53744bd5a7f198ab4
Ishan <ishan@mtlbigger.com>
2022-07-10 19:03:00 +0530

    declaring multiple variables and creating expressions using them

--- Section-3/HelloWorld.java
+++ Section-3/HelloWorld.java
@@ -2,6 +2,10 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello Ishan!");
         int myFirstNumber = 5;
-        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+
+        System.out.println((myFirstNumber*mySecondNumber) + myFirstNumber % (mySecondNumber / myThirdNumber));
     }
 }
